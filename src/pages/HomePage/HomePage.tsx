013c87203139e687d7446e2bfa16fafdd67d73a1
atomgx7 <atomgx7@gmail.com>
2024-03-21 16:38:16 -0600

    CENTERED THE FOKIN ARTICLE FINALLY, and added some logic to homepage.tsx

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
+
 import './HomePage.css';
 
 export function HomePage () {
+    const [url, setUrl] = useState('');
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        //logic of handleSubmit
+        console.log(url);
+        setUrl('');
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUrl(event.target.value);
+    }
 
     return (
         <article className="FormContainer">
             <h1>This is a home page</h1>
-            <form className="HomeForm">
-                <input type="text" required/>
+            <form className="HomeForm" onSubmit={handleSubmit}>
+                <input type="text" value={url} required placeholder="Url here" onChange={handleChange}/>
                 <button type="submit" className='HomeFormButton'>CONVERT</button> 
             </form>
         </article> 
