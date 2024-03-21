7e77b37d3d2bdf77bb4f0d64883b48ac8b13f3ba
Yahir Laptop HP <YahirGX5@gmail.com>
2024-03-20 23:50:24 -0600

    trying to center an article

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,14 @@
-
+import './HomePage.css';
 
 export function HomePage () {
 
     return (
-        <h1>This is a home page</h1>
+        <article className="FormContainer">
+            <h1>This is a home page</h1>
+            <form className="HomeForm">
+                <input type="text" required/>
+                <button type="submit" className='HomeFormButton'>CONVERT</button> 
+            </form>
+        </article> 
     )
 }
