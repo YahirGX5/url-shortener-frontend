7694208010a2c5d32532a26eebe796bb520536fc
Yahir Laptop HP <YahirGX5@gmail.com>
2024-03-22 22:00:02 -0600

    Created buttons for actions in url and added logic to UrlsViews, also centered the urls page

--- src/components/UrlsView/UrlsView.tsx
+++ src/components/UrlsView/UrlsView.tsx
@@ -1,9 +1,32 @@
-import { useState } from "react";
+import { useRef, useContext } from "react";
+import { urlsContext } from "../../contexts/UrlsContext";
 
-export function UrlsViews () {
+type UrlsViewsProps = {
+    originalUrl: string,
+    shortUrl: string
+}
+
+export function UrlsViews ({ originalUrl, shortUrl }: UrlsViewsProps) {
+    const urlRef = useRef(shortUrl);
+    const { urls, setUrls } = useContext(urlsContext);
+
+    const handleDelete = () => {
+        setUrls(urls.filter(url => url.shortUrl !== urlRef.current));
+
+    }
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(urlRef.current);
+    }
+
     return (
-        <article>
-
+        <article className="UrlContainer">
+            <a href={originalUrl} className="AnchorUrl">{urlRef.current}</a>
+            <section className="ButtonsContainer">
+                <button className="CopyButton" onClick={handleCopy}>📎</button>
+                <button className="ShareButton">Share</button>
+                <button className="DeleteButton" onClick={handleDelete}>Delete</button>
+            </section>
         </article>
     )
 }
--- src/pages/UrlsPage/UrlsPage.tsx
+++ src/pages/UrlsPage/UrlsPage.tsx
@@ -1,16 +1,19 @@
 import { urlsContext } from "../../contexts/UrlsContext";
 import { useContext } from "react";
+import { UrlsViews } from "../../components/UrlsView/UrlsView";
 import { Link } from "wouter";
 
+import './UrlsPage.css';
+
 export function UrlsPage () {
-    const { urls, setUrls } = useContext(urlsContext);
+    const { urls } = useContext(urlsContext);
 
     return (
-        <section>
+        <section className="UrlsContainer">
             <h1>This is urls page</h1>
             <Link href="/home">Home</Link>
             {urls ? 
-            <h1>Here goes the components for urls</h1> 
+                urls.map(({ originalUrl, shortUrl }) => <UrlsViews originalUrl={originalUrl} shortUrl={shortUrl}/>)
             : <h1>Nothing to see here</h1>} 
         </section> 
     )
